bd3bbd850a1511e560dfee531ee2cc145355cf5c
Sebastian Luna <eliaslunaslo@gmail.com>
2026-02-28 18:45:35 -0300

    Se debe corregir ejercicio 5 - tiene error de logica

---
+++ 5.js
@@ -0,0 +1,29 @@
+function longestPalindrome(s) {
+    const arr = s;
+    let longest = 0;
+    let sub = "";
+    //Caso de un unico caracter
+    if (arr.length <= 1) {
+        return arr[0];
+    }
+    for (let i = 0; i < arr.length; i++) {
+        let start = i;
+        let end = arr.length - 1;
+        let pointer = end - start;
+        while (pointer >= start) {
+            longest = pointer + 1;
+            if (arr[end] === arr[start]) {
+                end--;
+                start++;
+            }
+            if (pointer === start) {
+                sub = arr.slice(start, longest);
+            }
+            pointer--;
+        }
+    }
+    return sub;
+}
+;
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));
